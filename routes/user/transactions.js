0762951e71569efe1757ffcf756e8a157af4f403
modest_aryaman09 <61847192+AryamanMishra@users.noreply.github.com>
2021-11-11 00:09:46 +0530

    transactions.ejs completed

--- routes/user/transactions.js
+++ routes/user/transactions.js
@@ -8,7 +8,6 @@ const { v4: uuidv4 } = require('uuid');
 
 
 
-
 router.get('/users/:customer_id/transactions', (req,res) => {
     const customer_id = req.cookies.customer_id
     let sql = 
@@ -18,10 +17,41 @@ router.get('/users/:customer_id/transactions', (req,res) => {
     connectdb.query(sql, (err,result) => {
         if (err) throw err
         const transactions = result.rows
-        if (transactions.length === 0)
-            res.send('No transactions yet')
-        else 
-            res.render('user/transactions', {transactions,customer_id})
+        let product_id_sql = 
+        `
+            select a.product_id from order_details as a inner join transaction as b
+            on a.transaction_id = b.transaction_id
+        `
+        connectdb.query(product_id_sql, (err,result) => {
+            if (err) throw err
+            const product_id = result.rows
+            let product_id_array = []
+            for (let p of product_id)
+                product_id_array.push(p.product_id)
+            let ids = product_id_array.join("','", product_id_array)
+            let product_details_sql = 
+            `
+                select * from product where product_id in ('${ids}')
+            `
+            connectdb.query(product_details_sql, (err,result) => {
+                if (err) throw err 
+                let product_details = result.rows
+                let order_details_sql = 
+                `
+                    select * from order_details where product_id in ('${ids}')
+                `
+                connectdb.query(order_details_sql, (err,result) => {
+                    if (err) throw err
+                    let order_details = result.rows
+                    // console.log(product_details)
+                    if (transactions.length === 0)
+                        res.send('No transactions yet')
+                    else 
+                        res.render('user/transactions', {transactions,customer_id,product_details,order_details})
+                })
+            })
+        })
+        
     })
 })
 
